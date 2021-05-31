f599072bd3aefa8f4b745fbdcfdc02d00c4600e6
ante <apenava@playon.co>
2021-05-31 09:32:30 +0100

    changes

---
+++ app/controllers/fantasy.js
@@ -0,0 +1,10 @@
+import BaseController from 'f1-web/controllers/web/base';
+/**
+ * @public
+ *
+ * @class FantasyController
+ * @extends BaseController
+ *
+ * @since 1.0.0
+ */
+export default BaseController.extend({});
---
+++ dist/assets/tests.js
@@ -0,0 +1,31 @@
+'use strict';
+
+define("f1-web/tests/test-helper", ["f1-web/app", "f1-web/config/environment", "qunit", "@ember/test-helpers", "qunit-dom", "ember-qunit"], function (_app, _environment, QUnit, _testHelpers, _qunitDom, _emberQunit) {
+  "use strict";
+
+  (0, _testHelpers.setApplication)(_app.default.create(_environment.default.APP));
+  (0, _qunitDom.setup)(QUnit.assert);
+  (0, _emberQunit.start)();
+});
+define('f1-web/config/environment', [], function() {
+  var prefix = 'f1-web';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(decodeURIComponent(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+require('f1-web/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
